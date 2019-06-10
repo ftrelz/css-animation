fdf60c78b9c7e56871ee155759b6ca7a145a221a
Fred Trelz <ftrelz@users.noreply.github.com>
2019-06-10 14:32:48 -0500

    Code cleanup.

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -1,41 +1,40 @@
+function setPlatformSelected(platform) {
+    const platformElement = document.querySelector('.platform-' + platform)
+    const classes = platformElement.getAttribute('class')
+    platformElement.setAttribute('class', classes + ' platform-selected')
+}
+
+function setLogoBounce(platform) {
+    const logo = document.querySelector('.platform-' + platform + ' > .logo')
+    const classes = logo.getAttribute('class')
+    logo.setAttribute('class', classes + ' logo-bounce')
+}
 window.onload = function() {
     const bounceDelay = 300
 
-    const xboxOnClick = function(event) {
+    const xboxOnClick = function() {
         setTimeout(function() {
-            const logo = document.querySelector('.platform-xbox > .logo')
-            const classes = logo.getAttribute('class')
-            logo.setAttribute('class', classes + ' logo-bounce')
+            setLogoBounce('xbox')
         }, bounceDelay)
-        const platform = document.querySelector('.platform-xbox')
-        const classes = platform.getAttribute('class')
-        platform.setAttribute('class', classes + ' platform-selected')
+        setPlatformSelected('xbox')
     }
     document.querySelector('.platform-xbox').onclick = xboxOnClick
     document.querySelector('.platform-xbox > img').onclick = xboxOnClick
 
-    const pcOnClick = function(event) {
+    const pcOnClick = function() {
         setTimeout(function() {
-            const logo = document.querySelector('.platform-pc > .logo')
-            const classes = logo.getAttribute('class')
-            logo.setAttribute('class', classes + ' logo-bounce')
+            setLogoBounce('pc')
         }, bounceDelay)
-        const platform = document.querySelector('.platform-pc')
-        const classes = platform.getAttribute('class')
-        platform.setAttribute('class', classes + ' platform-selected')
+        setPlatformSelected('pc')
     }
     document.querySelector('.platform-pc').onclick = pcOnClick
     document.querySelector('.platform-pc > img').onclick = pcOnClick
 
-    const playstationOnClick = function(event) {
+    const playstationOnClick = function() {
         setTimeout(function() {
-            const logo = document.querySelector('.platform-playstation > .logo')
-            const classes = logo.getAttribute('class')
-            logo.setAttribute('class', classes + ' logo-bounce')
+            setLogoBounce('playstation')
         }, bounceDelay)
-        const platform = document.querySelector('.platform-playstation')
-        const classes = platform.getAttribute('class')
-        platform.setAttribute('class', classes + ' platform-selected')
+        setPlatformSelected('playstation')
     }
     document.querySelector('.platform-playstation').onclick = playstationOnClick
     document.querySelector('.platform-playstation > img').onclick = playstationOnClick
