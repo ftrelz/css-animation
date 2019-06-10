8dfd2fad20abf55005cf11e44658fbb41db6397b
Fred Trelz <ftrelz@users.noreply.github.com>
2019-06-10 14:25:23 -0500

    Fixed bug with clicking on logo instead of background.

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -1,32 +1,42 @@
 window.onload = function() {
     const bounceDelay = 300
-    document.getElementsByClassName('platform-xbox').item(0).onclick = function(event) {
+
+    const xboxOnClick = function(event) {
         setTimeout(function() {
             const logo = document.querySelector('.platform-xbox > .logo')
             const classes = logo.getAttribute('class')
             logo.setAttribute('class', classes + ' logo-bounce')
         }, bounceDelay)
-        const classes = event.target.getAttribute('class')
-        event.target.setAttribute('class', classes + ' platform-selected')
+        const platform = document.querySelector('.platform-xbox')
+        const classes = platform.getAttribute('class')
+        platform.setAttribute('class', classes + ' platform-selected')
     }
+    document.querySelector('.platform-xbox').onclick = xboxOnClick
+    document.querySelector('.platform-xbox > img').onclick = xboxOnClick
 
-    document.getElementsByClassName('platform-pc').item(0).onclick = function(event) {
+    const pcOnClick = function(event) {
         setTimeout(function() {
             const logo = document.querySelector('.platform-pc > .logo')
             const classes = logo.getAttribute('class')
             logo.setAttribute('class', classes + ' logo-bounce')
         }, bounceDelay)
-        const classes = event.target.getAttribute('class')
-        event.target.setAttribute('class', classes + ' platform-selected')
+        const platform = document.querySelector('.platform-pc')
+        const classes = platform.getAttribute('class')
+        platform.setAttribute('class', classes + ' platform-selected')
     }
+    document.querySelector('.platform-pc').onclick = pcOnClick
+    document.querySelector('.platform-pc > img').onclick = pcOnClick
 
-    document.getElementsByClassName('platform-playstation').item(0).onclick = function(event) {
+    const playstationOnClick = function(event) {
         setTimeout(function() {
             const logo = document.querySelector('.platform-playstation > .logo')
             const classes = logo.getAttribute('class')
             logo.setAttribute('class', classes + ' logo-bounce')
         }, bounceDelay)
-        const classes = event.target.getAttribute('class')
-        event.target.setAttribute('class', classes + ' platform-selected')
+        const platform = document.querySelector('.platform-playstation')
+        const classes = platform.getAttribute('class')
+        platform.setAttribute('class', classes + ' platform-selected')
     }
+    document.querySelector('.platform-playstation').onclick = playstationOnClick
+    document.querySelector('.platform-playstation > img').onclick = playstationOnClick
 }
